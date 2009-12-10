03a09b861cca61035c030d603cb07b929e87e9c4
Jan Wedekind <jan@wedesoft.de>
2009-12-10 14:31:15 +0000

    Use Kernel::require when testing

--- test/tc_int.rb
+++ test/tc_int.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-require 'multiarray'
+Kernel::require 'multiarray'
 
 class TC_Int < Test::Unit::TestCase
 
--- test/tc_sequence.rb
+++ test/tc_sequence.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-require 'multiarray'
+Kernel::require 'multiarray'
 
 class TC_Sequence < Test::Unit::TestCase
 
