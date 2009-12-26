c3a6d74c1b7f3d2a8baafc3265879d1607327d3c
Jan Wedekind <jan@wedesoft.de>
2009-12-26 22:10:55 +0100

    Added some tests for multi-dimensional arrays.

--- test/tc_multiarray.rb
+++ test/tc_multiarray.rb
@@ -22,11 +22,45 @@ class TC_MultiArray < Test::Unit::TestCase
     @@types = nil
   end
 
+  def test_default
+    for t in @@types
+      m = Hornetseye::MultiArray( t, 3, 2 ).default
+      assert_equal [ [ 0, 0, 0 ], [ 0, 0, 0 ] ], m.to_a
+    end
+  end
+
   def test_multiarray_new
     for t in @@types
       m = Hornetseye::MultiArray.new t, 3, 2
       m.set
       assert_equal [ [ 0, 0, 0 ], [ 0, 0, 0 ] ], m.to_a
+    end
+  end
+
+  def test_multiarray_to_s
+    for t in @@types
+      assert_equal "MultiArray.#{t.to_s.downcase}(3,2)",
+                   Hornetseye::MultiArray( t, 3, 2 ).to_s
+    end
+  end
+
+  def test_multiarray_inspect
+    for t in @@types
+      assert_equal "MultiArray.#{t.inspect.downcase}(3,2)",
+                   Hornetseye::MultiArray( t, 3, 2 ).inspect
+    end
+  end
+
+  def test_storage_size
+    for t in @@types
+      assert_equal t.delegate.storage_size * 3 * 2,
+                   Hornetseye::MultiArray( t, 3, 2 ).delegate.storage_size
+    end
+  end
+
+  def test_typecode
+    for t in @@types
+      assert_equal t, Hornetseye::MultiArray( t, 3, 2 ).typecode
     end
   end
 
