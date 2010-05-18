af91e0c0f95743f8096b2de48251880673131c04
Jan Wedekind <jan@wedesoft.de>
2010-05-18 19:02:17 +0100

    Minor changes

--- test/ts_multiarray.rb
+++ test/ts_multiarray.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
 require 'tc_object'
-require 'test/unit'
-begin
-  require 'rubygems'
-rescue LoadError
-end
-Kernel::require 'multiarray'
-
-class TC_Test < Test::Unit::TestCase
-
-  INT = Hornetseye::INT
-
-  def test_all
-    INT.new 3
-  end
-
-end
