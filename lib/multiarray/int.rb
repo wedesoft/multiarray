4ab3c83fc7aa9aa3d83d8f01e453f4270f14d55d
Jan Wedekind <jan@wedesoft.de>
2010-05-30 21:02:57 +0100

    Working on documentation

--- lib/multiarray/int.rb
+++ lib/multiarray/int.rb
@@ -22,17 +22,35 @@ module Hornetseye
 
     class << self
 
+      # Number of bits of this integer
       attr_accessor :bits
+
+      # Boolean indicating whether this is a signed integer or not
       attr_accessor :signed
 
+      # Memory type required to store elements of this type
+      #
+      # @return [Class] Returns +Malloc+.
+      #
+      # @private
       def memory
         Malloc
       end
 
+      # Get storage size to store an element of this type
+      #
+      # @return [Integer] Returns +1+.
+      #
+      # @private
       def storage_size
         ( bits + 7 ).div 8
       end
 
+      # Get default value for elements of this type
+      #
+      # @return [Object] Returns +false+.
+      #
+      # @private
       def default
         0
       end
@@ -45,6 +63,9 @@ module Hornetseye
         end
       end
 
+      # Directive for packing/unpacking elements of this type
+      #
+      # @private
       def directive
         case [ bits, signed ]
         when [  8, true  ]
@@ -132,6 +153,11 @@ module Hornetseye
         end
       end
 
+      # Comparison operator
+      #
+      # @param [Object] other Other object to compare with.
+      #
+      # @return [FalseClass,TrueClass] Result of comparison.
       def ==( other )
         other.is_a? Class and other < INT_ and
           bits == other.bits and signed == other.signed
@@ -185,6 +211,18 @@ module Hornetseye
 
   end
 
+  # Create a class deriving from +INT_+
+  #
+  # he aprameters +bits+ and +signed+ are assigned to the corresponding
+  # attributes of the resulting class.
+  #
+  # @param [Integer] bits Number of bits of native integer.
+  # @param [FalseClass,TrueClass] signed Specify +UNSIGNED+ or +SIGNED+ here.
+  # @return [Class] A class deriving from +INT_+.
+  #
+  # @see INT_
+  # @see INT_.bits
+  # @see INT_.signed
   def INT( bits, signed )
     retval = Class.new INT_
     retval.bits = bits
@@ -194,16 +232,42 @@ module Hornetseye
 
   module_function :INT
 
+  # Boolean constant to use as a parameter for creating integer classes
+  #
+  # The value is +false+.
+  #
+  # @see #INT
   UNSIGNED = false
+
+  # Boolean constant to use as a parameter for creating integer classes
+  #
+  # The value is +true+.
+  #
+  # @see #INT
   SIGNED   = true
 
+  # 8-bit signed integer
   BYTE  = INT  8, SIGNED
+
+  # 8-bit unsigned integer
   UBYTE = INT  8, UNSIGNED
+
+  # 16-bit signed integer
   SINT  = INT 16, SIGNED
+
+  # 16-bit unsigned integer
   USINT = INT 16, UNSIGNED
+
+  # 32-bit signed integer
   INT   = INT 32, SIGNED
+
+  # 32-bit unsigned integer
   UINT  = INT 32, UNSIGNED
+
+  # 64-bit signed integer
   LONG  = INT 64, SIGNED
+
+  # 64-bit unsigned integer
   ULONG = INT 64, UNSIGNED
 
 end
