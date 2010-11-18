8751918d3f23175b68499c768c396574d2d069e4
Jan Wedekind <jan@wedesoft.de>
2010-11-18 14:11:47 +0000

    Working on documentation

--- lib/multiarray/gcctype.rb
+++ lib/multiarray/gcctype.rb
@@ -17,12 +17,25 @@
 # Namespace of Hornetseye computer vision library
 module Hornetseye
 
+  # Class for translating native types from Ruby to C
+  #
+  # @private
   class GCCType
 
+    # Construct GCC type
+    #
+    # @param [Class] typecode Native type (e.g. +UBYTE+).
+    #
+    # @private
     def initialize( typecode )
       @typecode = typecode
     end
 
+    # Get C identifier for native type
+    #
+    # @return [String] String with valid C syntax to declare type.
+    #
+    # @private
     def identifier
       case @typecode
       when nil
@@ -56,6 +69,11 @@ module Hornetseye
       end
     end
 
+    # Get array of C identifiers for native type
+    #
+    # @return [Array<String>] Array of C declarations for the elements of the type.
+    #
+    # @private
     def identifiers
       if @typecode < Composite
         GCCType.new( @typecode.element_type ).identifiers * @typecode.num_elements
@@ -64,6 +82,14 @@ module Hornetseye
       end
     end
 
+    # Get code for converting Ruby VALUE to C value
+    #
+    # This method returns a nameless function. The nameless function is used for
+    # getting the code to convert a given parameter to a C value of this type.
+    #
+    # @return [Proc] Nameless function accepting a C expression to be converted.
+    #
+    # @private
     def r2c
       case @typecode
       when BOOL
