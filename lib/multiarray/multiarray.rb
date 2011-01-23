a4a33fa2233c81b9e49e66f4d7797a481a1ae693
Jan Wedekind <jan@wedesoft.de>
2011-01-23 22:56:11 +0000

    Added Laplacian of Gaussian filter

--- lib/multiarray/multiarray.rb
+++ lib/multiarray/multiarray.rb
@@ -74,12 +74,44 @@ module Hornetseye
       # shape of the array is determined. Finally the elements are coopied to the
       # resulting array.
       #
-      # @param [Array<Object>] *args
+      # @param [Array<Object>] *args The array elements.
       #
       # @return [Node] Uniform multi-dimensional array.
       def []( *args )
         target = Node.fit args
         target[ *args ]
+      end
+
+      # Compute Laplacian of Gaussian filter
+      #
+      # @param [Float] sigma Spread of filter.
+      # @param [Integer] size Size of filter (*e.g.* 9 for 9x9 filter)
+      #
+      # @return[Node] The filter.
+      def laplacian_of_gaussian( sigma = 1.4, size = 9 )
+        def erf( x, sigma )
+          0.5 * Math.erf( x / ( Math.sqrt( 2.0 ) * sigma.abs ) )
+        end
+        def gauss_gradient( x, sigma )
+          -x / ( Math.sqrt( 2.0 * Math::PI * sigma.abs**5 ) ) *
+            Math.exp( -x**2 / ( 2.0 * sigma**2 ) )
+        end
+        retval = new DFLOAT, size, size
+        sum = 0
+        for y in 0 .. size - 1
+          y0 = y - 0.5 * size
+          y1 = y0 + 1
+          y_grad_diff = gauss_gradient( y1, sigma ) - gauss_gradient( y0, sigma )
+          y_erf_diff = erf( y1, sigma ) - erf( y0, sigma )
+          for x in 0..size-1
+            x0 = x - 0.5 * size
+            x1 = x0 + 1
+            x_grad_diff = gauss_gradient( x1, sigma ) - gauss_gradient( x0, sigma )
+            x_erf_diff = erf( x1, sigma ) - erf( x0, sigma )
+            retval[ y, x ] = y_grad_diff * x_erf_diff + y_erf_diff * x_grad_diff
+          end
+        end
+        retval
       end
 
     end
