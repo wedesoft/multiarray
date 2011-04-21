88b022ffca95997e9f4ec866ee4842b2ab0db255
Jan Wedekind <jan@wedesoft.de>
2011-04-21 14:48:05 +0200

    Removed argument structure in C

--- lib/multiarray/gcccontext.rb
+++ lib/multiarray/gcccontext.rb
@@ -138,46 +138,26 @@ module Hornetseye
     # @private
     def function( descriptor, *param_types )
       @instructions << <<EOS
-struct Arguments
-{
-#{
+VALUE #{descriptor}( #{
 param_types.collect do |t|
   t.identifiers
 end.flatten.collect_with_index do |ident,i|
-  "  #{ident} param#{i};"
-end.join( "\n" )
-}
-};
-
-VALUE #{descriptor}( void *args )
+  "#{ident} param#{i}"
+end.join ', '
+} )
 {
-#{
-param_types.collect do |t|
-  t.identifiers
-end.flatten.collect_with_index do |ident,i|
-  "  #{ident} param#{i} = ((struct Arguments *)args)->param#{i};"
-end.join( "\n" )
-}
 EOS
 
       @wrappers << <<EOS
 VALUE wrap#{descriptor.capitalize}( int argc, VALUE *argv, VALUE rbSelf )
 {
-  struct Arguments args;
-#{
+  #{descriptor}( #{
 param_types.collect do |t|
   t.r2c
 end.flatten.collect_with_index do |conv,i|
-  "  args.param#{i} = #{conv.call "argv[ #{i} ]"};"
-end.join( "\n" )
-}
-#{
-if RUBY_VERSION < '1.9.0'
-  "  #{descriptor}( &args );"
-else
-  "  rb_thread_blocking_region( &#{descriptor}, &args, RUBY_UBF_PROCESS, NULL );"
-end
-}
+  "#{conv.call "argv[#{i}]"}"
+end.join ', '
+  } );
   return Qnil;
 }
 EOS
@@ -257,6 +237,7 @@ EOS
       gcc = "#{LDSHARED} #{CFLAGS} -o #{DIRNAME}/#{@lib_name}.#{DLEXT} " +
             "#{DIRNAME}/#{@lib_name}.c #{LIBRUBYARG}"
       # puts template
+      # puts gcc
       raise "The following command failed: #{gcc}" unless system gcc
       require "#{DIRNAME}/#{@lib_name}"
     end
